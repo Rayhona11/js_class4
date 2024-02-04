576800e720e07516544fe538108bbefc6e2b58df
Rayhona <rayhonamirzoyeva.ru>
2024-02-04 20:26:09 +0500

    fcgfacx

--- script2.js
+++ script2.js
@@ -1,10 +1,10 @@
-// DOM BOM 
+ // DOM BOM 
 
 // Content bilan ishlash
  let h = document.querySelector('h1')
  let ul = document.querySelector('ul')
 
- console.dir(document)
+ console.dir(ul)
 //  innerHTML, innerText,outerHTML, outerText, textContent
 // before(), after(), prepend(), append(), remove(), createElement()
 console.log(h.innerText);
@@ -32,9 +32,17 @@ h.remove()
 // Agar 25 < 50 prepend
 // Agar 50 < 75 append
 // Agar 75 < 100 after
-let num = 25;
- if (25 < num){
-    console.log('to`g`ri');
- }
- console.log(append);
 
+for (let a = 0; a < 100; a++){
+    let li = document.createElement('li')
+    li.innerText = a 
+    if (a < 25){
+        ul.before(li)
+    } else if (a >= 25 && a < 50) {
+      ul.prepend(li)
+    }else if (a >= 50 && a < 75) {
+        ul.append(li)
+    }else {
+        ul.after(li)
+    }
+}
---
+++ script3.js
@@ -0,0 +1,29 @@
+// let i = {
+//     name:"Rayhona",
+//     surname:"Mirzoyeva",
+//     age:15,
+//     job:"Programmer",
+//     class:"9-'A' ",
+//     school:27
+// }
+// console.log(i.name);
+
+// Homework
+//  for (let a = 0; a < 100; a++){
+//     let li = document.createElement('li')
+//     li.innerText = a 
+//     if (a < 25){
+//         ul.before(li)
+//     } else if (a >= 25 && a < 50) {
+//       ul.prepend(li)
+//     }else if (a >= 50 && a < 75) {
+//         ul.append(li)
+//     }else {
+//         ul.after(li)
+//     }
+// }
+
+for (let b = 0; b < 100; b++){
+    let form = document.createElement('form')
+    form.innerText = b 
+}
